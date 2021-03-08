bb16937012d20c2f75a87800293e7e2766028bdd
HypnoticSiege <65150090+HypnoticSiege@users.noreply.github.com>
2021-03-07 20:32:31 -0500

    Update relationship.js

--- relationship.js
+++ relationship.js
@@ -4,16 +4,16 @@ const prompt = require('prompt-sync')();
 const relationship = prompt('What is your current Relationship Status? ');
 
 //If the question is answered 'single'
-if (relationship === 'single') {
+if (relationship == 'single') {
     console.log('Hahaha you are single')
 }
 
 //If the question is answered 'taken'
-if (relationship === 'taken') {
+if (relationship == 'taken') {
     console.log('Yay you got someone, good job :D')
 }
 
 //If the question is answered 'talking'
-if (relationship === 'talking') {
+if (relationship == 'talking') {
     console.log('OooOOooo someone\'s got company')
 }
